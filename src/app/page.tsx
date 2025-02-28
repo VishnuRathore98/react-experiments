6dfb4c1046d4d20df8d21d4d8102217f508ec13e
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-02-28 22:56:12 +0530

    Multiple profile return

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,25 @@ export default function Home() {
   );
 }
 
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
 
+export function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
 
 /* Boiler plate code:
 
